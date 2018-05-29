0fb4fc5ea47966ad48527ba2ae14dd2c24c186ba
Unknown <tom@dlnet.us>
2018-05-29 22:09:06 +0200

    added mouse events

--- WallpaperAnimator/Form1.Designer.cs
+++ WallpaperAnimator/Form1.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(291, 186);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(305, 148);
+            this.ClientSize = new System.Drawing.Size(779, 533);
+            this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
@@ -47,6 +59,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WallpaperAnimator/Form1.cs
+++ WallpaperAnimator/Form1.cs
@@ -2,8 +2,13 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
 using WindowUtils;
 
 namespace WallpaperAnimator
@@ -13,10 +18,92 @@ namespace WallpaperAnimator
         private double _angle;
         private Screen _screen;
         private SolidBrush _brush;
+        private IKeyboardMouseEvents _events = Hook.GlobalEvents();
+
+        [DllImport("user32.dll")]
+        static extern int GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetClassName(int hWnd, StringBuilder lpClassName, int nMaxCount);
+
+        private MethodInfo _onClick = typeof(Control).GetRuntimeMethods().Single(m => m.Name == "OnClick");
+
+        private bool DesktopFocus
+        {
+            get
+            {
+                var className = new StringBuilder(256);
+
+                if (GetClassName(GetForegroundWindow(), className, 256) > 0)
+                {
+                    string cName = className.ToString();
+                    return cName == "Progman" || cName == "WorkerW";
+                }
+
+                return false;
+            }
+        }
 
         public Form1()
         {
             InitializeComponent();
+
+            void Click (MouseEventArgs e)
+            {
+                if (DesktopFocus)
+                {
+                    var clickedControl = false;
+
+                    for (var index = 0; index < Controls.Count; index++)
+                    {
+                        Control control = Controls[index];
+
+                        if (new Rectangle(control.Location, control.ClientSize).Contains(e.Location))
+                        {
+                            clickedControl = true;
+                            if (control is Button btn)
+                                btn.PerformClick();
+                            else
+                                _onClick.Invoke(control, new object[] { e });
+                        }
+                    }
+
+                    if (!clickedControl)
+                        OnClick(e);
+                }
+            }
+
+            var wasDown = false;
+
+            _events.MouseDown += (o, e) =>
+            { 
+                wasDown = true;
+
+                if (DesktopFocus)
+                {
+                    OnMouseDown(e);
+                }
+            };
+            _events.MouseUp += (o, e) =>
+            {
+                if (wasDown)
+                    Click(e);
+
+                wasDown = false;
+
+                if (DesktopFocus)
+                {
+                    OnMouseUp(e);
+                }
+            };
+            _events.MouseMove += (o, e) =>
+            {
+                if (DesktopFocus)
+                {
+                    Focus();
+                    OnMouseMove(e);
+                }
+            };
 
             CreateResetWallpaperScritp();
         }
@@ -25,48 +112,49 @@ namespace WallpaperAnimator
         {
             _brush = new SolidBrush(Color.Black);
             _screen = Screen.FromPoint(Location);
-            
+
             ClientSize = _screen.Bounds.Size;
-            Location = new Point();
+            Location = _screen.Bounds.Location;
 
             this.SetAsWallpaper();
 
             new Thread(() =>
-            {
-                var invoker = new MethodInvoker(() =>
                 {
-                    Invalidate();
-                    for (var index = 0; index < Controls.Count; index++)
+                    try
                     {
-                        Control control = Controls[index];
-                        control.Invalidate();
-                    }
-                });
-                try
-                {
-                    var span = TimeSpan.FromMilliseconds(16);
+                        var span = TimeSpan.FromMilliseconds(16);
 
-                    while (true)
-                    {
-                        //var now = DateTime.Now;
-
-                        if (IsHandleCreated && Created)
+                        while (true)
                         {
-                            _angle -= 1.5;
+                            //var now = DateTime.Now;
 
-                            Invoke(invoker);
+                            if (IsHandleCreated && Created)
+                            {
+                                _angle -= 1.5;
+
+                                Invoke((MethodInvoker) (() =>
+                                {
+                                    Invalidate();
+                                    for (var index = 0; index < Controls.Count; index++)
+                                    {
+                                        Control control = Controls[index];
+                                        control.Invalidate();
+                                    }
+                                }));
+                            }
+
+                            //var renderTime = DateTime.Now - now;
+                            //var sleepTime = span - renderTime;
+
+                            //if (sleepTime > TimeSpan.Zero)
+                            Thread.Sleep(span); //sleepTime);
                         }
-
-                        //var renderTime = DateTime.Now - now;
-                        //var sleepTime = span - renderTime;
-
-                        //if (sleepTime > TimeSpan.Zero)
-                        Thread.Sleep(span);//sleepTime);
                     }
-                }
-                catch { }
-            })
-            { IsBackground = true }.Start();
+                    catch
+                    {
+                    }
+                })
+                {IsBackground = true}.Start();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -145,6 +233,12 @@ namespace WallpaperAnimator
                 }
             }
             catch { }*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Hue(new Random().NextDouble() * 360);
+            //MessageBox.Show("asdasdad");
         }
     }
 }
