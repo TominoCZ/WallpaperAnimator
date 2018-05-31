3472f446f83bbb0d2a9ea9bc68740a242d171232
Unknown <tom@dlnet.us>
2018-05-31 23:40:59 +0200

    kewl effecct

--- WallpaperAnimator/Program.cs
+++ WallpaperAnimator/Program.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Windows.Forms;
-using Gma.System.MouseKeyHook;
+﻿using Gma.System.MouseKeyHook;
 using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
 using WindowUtils;
 using Math = System.Math;
-using Mouse = System.Windows.Input.Mouse;
 
 namespace WallpaperAnimator
 {
@@ -22,25 +20,16 @@ namespace WallpaperAnimator
     {
         private static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-
             using (var w = new Game())
             {
+                w.SetAsWallpaper();
                 w.Run(20);
             }
         }
     }
 
-    class Game : GameWindow
+    internal class Game : GameWindow
     {
-        [DllImport("user32.dll")]
-        private static extern int GetForegroundWindow();
-
-        [DllImport("user32.dll")]
-        private static extern int GetClassName(int hWnd, StringBuilder lpClassName, int nMaxCount);
-
         public static Game Instance;
         public static Random Random = new Random();
 
@@ -51,14 +40,17 @@ namespace WallpaperAnimator
         private StringBuilder _className;
         private Point _lastMouse;
         private Size _lastWindowSize;
-        private bool _mouseDown;
         private float _angle, _prevAngle;
+        private bool _canUpdate = true;
+        private bool _wasFocused = false;
 
         private Stopwatch _updateTimer;
 
-        private static GraphicsMode _gMode = new GraphicsMode(32, 16, 0, 0, 0, 2, false);
+        private static GraphicsMode _gMode = new GraphicsMode(32, 16, 0, 8, 0, 2, false);
 
         private static List<MouseButtons> _down = new List<MouseButtons>();
+
+        private static List<string> _processExceptions = new List<string>();
 
         public Game() : base(1, 1, _gMode, "", GameWindowFlags.Default, DisplayDevice.Default, 3, 3, GraphicsContextFlags.ForwardCompatible)
         {
@@ -66,10 +58,6 @@ namespace WallpaperAnimator
 
             WindowState = WindowState.Maximized;
             WindowBorder = WindowBorder.Hidden;
-
-            TargetRenderFrequency = 60;
-
-            this.SetAsWallpaper();
 
             Init();
         }
@@ -85,37 +73,72 @@ namespace WallpaperAnimator
                 if (!_down.Contains(e.Button))
                     _down.Add(e.Button);
 
-                //if (!IsDesktopFocused())
-                //return;
+                if (!IsDesktopFocused())
+                    return;
 
-                //MouseButton btn = e.Button == MouseButtons.Right ? MouseButton.Right : MouseButton.Left;
+                MouseButton btn = e.Button == MouseButtons.Right ? MouseButton.Right : MouseButton.Left;
 
-                //OnMouseDown(new MouseButtonEventArgs(e.X, e.Y, btn, true));
+                OnMouseDown(new MouseButtonEventArgs(e.X, e.Y, btn, true));
             };
             _events.MouseUp += (o, e) =>
-            {
-                // if (!IsDesktopFocused())
-                // return;
-
-                _down.Remove(e.Button);
-
-                // MouseButton btn = e.Button == MouseButtons.Right ? MouseButton.Right : MouseButton.Left;
-
-                //OnMouseUp(new MouseButtonEventArgs(e.X, e.Y, btn, true));
-            };
-            _events.MouseMove += (o, e) =>
             {
                 if (!IsDesktopFocused())
                     return;
 
-                OnMouseMove(new MouseMoveEventArgs(e.X, e.Y, 0, 0));
+                _down.Remove(e.Button);
+
+                MouseButton btn = e.Button == MouseButtons.Right ? MouseButton.Right : MouseButton.Left;
+
+                OnMouseUp(new MouseButtonEventArgs(e.X, e.Y, btn, true));
             };
+            _events.MouseMove += (o, e) =>
+            {
+                _lastMouse = e.Location;
+            };
+
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            if (!File.Exists("wanim.cfg"))
+            {
+                File.WriteAllText("wanim.cfg", "//here you can specify which processes will make WallpaperAnimator not render if running");
+                return;
+            }
+
+            try
+            {
+                var lines = File.ReadAllLines("wanim.cfg");
+
+                foreach (var t in lines)
+                {
+                    var line = t.Replace(" ", "");
+
+                    if (line.StartsWith("//") || string.IsNullOrEmpty(line))
+                        continue;
+
+                    line = Path.GetFileNameWithoutExtension(line).ToLower();
+
+                    if (line == "explorer")
+                        continue;
+
+                    _processExceptions.Add(line);
+                }
+            }
+            catch
+            {
+            }
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
+            if (!_canUpdate)
+                return;
+
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            GL.Enable(EnableCap.LineSmooth);
+            //GL.Enable(EnableCap.LineSmooth);
+            //GL.Enable(EnableCap.PolygonSmooth);
             GL.Enable(EnableCap.CullFace);
             GL.Enable(EnableCap.Blend);
             GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
@@ -127,8 +150,24 @@ namespace WallpaperAnimator
 
             DrawSine(20, 20, 0.5f, Height, deltaTime);
 
-            if (_down.Contains(MouseButtons.Left) && IsDesktopFocused())
-                SpawnParticles(_lastMouse, 2);
+            if (_down.Contains(MouseButtons.Left))
+            {
+                if (IsDesktopFocused())
+                {
+                    if (!_wasFocused)
+                    {
+                        _down.Clear();
+                    }
+                    else
+                        SpawnParticles(_lastMouse, 2);
+
+                    _wasFocused = true;
+                }
+                else if (_wasFocused)
+                {
+                    _wasFocused = false;
+                }
+            }
 
             _particleManager.Render(deltaTime);
 
@@ -137,9 +176,15 @@ namespace WallpaperAnimator
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
-            _prevAngle = _angle;
-            _angle -= 4f;
+            if (!(_canUpdate = CanUpdate()))
+            {
+                TargetRenderFrequency = 5;
+                return;
+            }
 
+            TargetRenderFrequency = 60;
+
+            //check screen size
             if (_lastWindowSize != Size)
             {
                 GL.Viewport(0, 0, Width, Height);
@@ -152,18 +197,20 @@ namespace WallpaperAnimator
                 _lastWindowSize = Size;
             }
 
-            if (_mouseDown)
-                SpawnParticles(_lastMouse, 4);
+            _prevAngle = _angle;
+            _angle -= 4f;
+
+            var step = Width / 16f;
+
+            for (int i = 0; i <= 16; i++)
+            {
+                var x = step * i;
+
+                SpawnFireParticles(x, Height, 1);
+            }
 
             _particleManager.Update();
             _updateTimer.Restart();
-        }
-
-        protected override void OnMouseMove(MouseMoveEventArgs e)
-        {
-            base.OnMouseMove(e);
-
-            _lastMouse = e.Position;
         }
 
         private void DrawSine(float pointWidth, float pointHeight, float sineHeightRatio, float canvasHeight, float deltaTime, float phaseOffset = 0, float waveLengthRatio = 1)
@@ -192,7 +239,17 @@ namespace WallpaperAnimator
                 GL.Translate(X, Y, 0);
                 GL.Scale(pointWidth, pointHeight, 0);
 
-                GL.Begin(PrimitiveType.Quads);
+                GL.Begin(PrimitiveType.Polygon);
+
+                GL.Color4(c.X, c.Y, c.Z, 0.2);
+                GL.Vertex2(0, 0);
+                GL.Vertex2(0, 1);
+                GL.Vertex2(1, 1);
+                GL.Vertex2(1, 0);
+
+                GL.End();
+
+                GL.Begin(PrimitiveType.LineLoop);
 
                 GL.Color3(c.X, c.Y, c.Z);
                 GL.Vertex2(0, 0);
@@ -223,20 +280,39 @@ namespace WallpaperAnimator
                 {
                     particle = new ParticleTriangle(p.X + offX, p.Y + offY, dir.X, dir.Y, Random.Next(15, 25),
                             8 + (float)Random.NextDouble() * 24)
-                    { Traction = 1.15f };
+                    { Acceleration = 0.9f };
                 }
                 else if (r == 2)
                 {
                     particle = new ParticleSquare(p.X + offX, p.Y + offY, dir.X, dir.Y, Random.Next(15, 25),
                             8 + (float)Random.NextDouble() * 24)
-                    { Traction = 1.15f };
+                    { Acceleration = 0.9f };
                 }
                 else
                 {
                     particle = new ParticleCircle(p.X + offX, p.Y + offY, dir.X, dir.Y, Random.Next(15, 25),
                             4 + (float)Random.NextDouble() * 20)
-                    { Traction = 1.15f };
+                    { Acceleration = 0.9f };
                 }
+
+                _particleManager.AddParticle(particle);
+            }
+        }
+
+        private void SpawnFireParticles(float x, float y, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var offX = -25 + (float)Random.NextDouble() * 50;
+                var offY = -5 + (float)Random.NextDouble() * 10;
+
+                var dir = Vector2.Normalize(new Vector2(offX, offY)) * 1.5f;
+
+                var size = 8 + (float)Random.NextDouble() * 24;
+
+                var particle = new ParticleSquare(x + offX, y + (float)Math.Sqrt(size * size + size * size), dir.X, -Math.Abs(dir.Y * 2f), Random.Next(20, 40),
+                        size)
+                { Acceleration = 1f };
 
                 _particleManager.AddParticle(particle);
             }
@@ -246,7 +322,7 @@ namespace WallpaperAnimator
         {
             _className.Clear();
 
-            if (GetClassName(GetForegroundWindow(), _className, 256) > 0)
+            if (W32.GetClassName(W32.GetForegroundWindow(), _className, 256) > 0)
             {
                 string cName = _className.ToString();
                 return cName == "Progman" || cName == "WorkerW";
@@ -254,9 +330,29 @@ namespace WallpaperAnimator
 
             return false;
         }
+
+        private bool CanUpdate()
+        {
+            for (var index = 0; index < _processExceptions.Count; index++)
+            {
+                var exception = _processExceptions[index];
+                var arr = Process.GetProcessesByName(exception);
+
+                for (var i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].ProcessName.ToLower() == exception)
+                        return false;
+                }
+            }
+
+            var wnd = W32.GetForegroundWindow();
+            var placement = WindowUtil.GetPlacement(wnd);
+
+            return placement.showCmd != WindowUtil.ShowWindowCommands.Maximized || wnd == WindowInfo.Handle;
+        }
     }
 
-    class ParticleManager
+    internal class ParticleManager
     {
         private readonly List<Particle> _particles = new List<Particle>();
 
@@ -287,11 +383,11 @@ namespace WallpaperAnimator
         }
     }
 
-    class ParticleSquare : Particle
+    internal class ParticleSquare : Particle
     {
         public ParticleSquare(float x, float y, float mx, float my, int maxAge, float size) : base(x, y, mx, my, maxAge, size)
         {
-            var mult = Math.Min(32 / size, 4);
+            var mult = Math.Max(Math.Min(32 / size, 4), 1);
 
             Mx *= mult;
             My *= mult;
@@ -315,6 +411,16 @@ namespace WallpaperAnimator
             GL.Rotate(deltaAngle, 0, 0, 1);
             GL.Scale(deltaSize, deltaSize, 0);
 
+            GL.Begin(PrimitiveType.Polygon);
+
+            GL.Color4(c.X, c.Y, c.Z, deltaAlpha * 0.2);
+            GL.Vertex2(-0.5, -0.5);
+            GL.Vertex2(-0.5, 0.5);
+            GL.Vertex2(0.5, 0.5);
+            GL.Vertex2(0.5, -0.5);
+
+            GL.End();
+
             GL.Begin(PrimitiveType.LineLoop);
 
             GL.Color4(c.X, c.Y, c.Z, deltaAlpha);
@@ -324,11 +430,25 @@ namespace WallpaperAnimator
             GL.Vertex2(0.5, -0.5);
 
             GL.End();
+
+            GL.Begin(PrimitiveType.Polygon);
+            PutCircle(-0.5f, -0.5f, .2f);
+            GL.End();
+            GL.Begin(PrimitiveType.Polygon);
+            PutCircle(-0.5f, 0.5f, .2f);
+            GL.End();
+            GL.Begin(PrimitiveType.Polygon);
+            PutCircle(0.5f, 0.5f, .2f);
+            GL.End();
+            GL.Begin(PrimitiveType.Polygon);
+            PutCircle(0.5f, -0.5f, .2f);
+            GL.End();
+
             GL.PopMatrix();
         }
     }
 
-    class ParticleTriangle : Particle
+    internal class ParticleTriangle : Particle
     {
         public ParticleTriangle(float x, float y, float mx, float my, int maxAge, float size) : base(x, y, mx, my, maxAge, size)
         {
@@ -350,15 +470,24 @@ namespace WallpaperAnimator
 
             var c = Hue.Create(deltaX / Game.Instance.Width * 360);
 
+            var x = 0.43301270189222f;
+
             GL.PushMatrix();
 
             GL.Translate(deltaX, deltaY, 0);
             GL.Rotate(deltaAngle, 0, 0, 1);
             GL.Scale(deltaSize, deltaSize, 0);
 
-            GL.Begin(PrimitiveType.LineLoop);
+            GL.Begin(PrimitiveType.Polygon);
 
-            var x = 0.43301270189222D;
+            GL.Color4(c.X, c.Y, c.Z, deltaAlpha * 0.2);
+            GL.Vertex2(0, -x);
+            GL.Vertex2(-0.5, x);
+            GL.Vertex2(0.5, x);
+
+            GL.End();
+
+            GL.Begin(PrimitiveType.LineLoop);
 
             GL.Color4(c.X, c.Y, c.Z, deltaAlpha);
             GL.Vertex2(0, -x);
@@ -366,11 +495,22 @@ namespace WallpaperAnimator
             GL.Vertex2(0.5, x);
 
             GL.End();
+
+            GL.Begin(PrimitiveType.Polygon);
+            PutCircle(0, -x, .2f);
+            GL.End();
+            GL.Begin(PrimitiveType.Polygon);
+            PutCircle(-0.5f, x, .2f);
+            GL.End();
+            GL.Begin(PrimitiveType.Polygon);
+            PutCircle(0.5f, x, .2f);
+
+            GL.End();
             GL.PopMatrix();
         }
     }
 
-    class ParticleCircle : Particle
+    internal class ParticleCircle : Particle
     {
         public ParticleCircle(float x, float y, float mx, float my, int maxAge, float size) : base(x, y, mx, my, maxAge, size)
         {
@@ -398,26 +538,24 @@ namespace WallpaperAnimator
             GL.Rotate(deltaAngle, 0, 0, 1);
             GL.Scale(deltaSize, deltaSize, 0);
 
+            GL.Begin(PrimitiveType.Polygon);
+
+            GL.Color4(c.X, c.Y, c.Z, deltaAlpha * 0.2);
+            PutCircle(0, 0, 0, 24);
+
+            GL.End();
+
             GL.Begin(PrimitiveType.LineLoop);
 
             GL.Color4(c.X, c.Y, c.Z, deltaAlpha);
-
-            for (int i = 0; i < 24; i++)
-            {
-                var a = i / 24f;
-
-                var x = Math.Cos(a * MathHelper.TwoPi) / 2;
-                var y = Math.Sin(a * MathHelper.TwoPi) / 2;
-
-                GL.Vertex2(x, y);
-            }
+            PutCircle(0, 0, 0, 24);
 
             GL.End();
             GL.PopMatrix();
         }
     }
 
-    class Particle
+    internal class Particle
     {
         public float X, Y, PrevX, PrevY;
 
@@ -431,7 +569,7 @@ namespace WallpaperAnimator
 
         public float Angle, PrevAngle;
 
-        public float Traction = 1;
+        public float Acceleration = 1;
 
         protected int _direction;
 
@@ -446,7 +584,7 @@ namespace WallpaperAnimator
             My = my;
 
             MaxAge = maxAge;
-            StartSize = PrevSize = Size = size;
+            StartSize = size;
 
             PrevAlpha = Alpha = 1;
 
@@ -465,6 +603,11 @@ namespace WallpaperAnimator
 
             PrevAngle = Angle;
 
+            if (Age <= 2)
+            {
+                Size = Age / 2f * StartSize;
+            }
+
             if (Age++ >= MaxAge)
             {
                 if (Size <= 1)
@@ -476,23 +619,36 @@ namespace WallpaperAnimator
                 }
             }
 
-            var mult = (float)Math.Min(Math.Sqrt(Mx * Mx + My * My), 4) * 4 * _direction;
+            var mult = (float)Math.Min(Math.Sqrt(Mx * Mx + My * My), 4) * 3 * _direction;
 
             Angle += mult;
 
             X += Mx;
             Y += My;
 
-            Mx *= 0.9875f / Traction;
-            My *= 0.9875f / Traction;
+            Mx *= 0.9875f * Acceleration;
+            My *= 0.9875f * Acceleration;
         }
 
         public virtual void Render(float deltaTime)
         {
         }
+
+        protected void PutCircle(float centerX = 0, float centerY = 0, float size = 1, int points = 10)
+        {
+            for (int i = points - 1; i >= 0; i--)
+            {
+                var a = i / (float)points;
+
+                var x = Math.Cos(a * MathHelper.TwoPi) / 2 * size;
+                var y = Math.Sin(a * MathHelper.TwoPi) / 2 * size;
+
+                GL.Vertex2(x + centerX, y + centerY);
+            }
+        }
     }
 
-    static class Hue
+    internal static class Hue
     {
         public static Vector3 Create(double value)
         {
@@ -570,6 +726,39 @@ namespace WallpaperAnimator
             p.Offset(offsetX, offsetY);
 
             return p;
+        }
+
+        public static WINDOWPLACEMENT GetPlacement(IntPtr hwnd)
+        {
+            var placement = new WINDOWPLACEMENT();
+            placement.length = Marshal.SizeOf(placement);
+            GetWindowPlacement(hwnd, ref placement);
+            return placement;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetWindowPlacement(
+            IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        [Serializable]
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public ShowWindowCommands showCmd;
+            public Point ptMinPosition;
+            public Point ptMaxPosition;
+            public Rectangle rcNormalPosition;
+        }
+
+        public enum ShowWindowCommands : int
+        {
+            Hide = 0,
+            Normal = 1,
+            Minimized = 2,
+            Maximized = 3,
         }
     }
 }
